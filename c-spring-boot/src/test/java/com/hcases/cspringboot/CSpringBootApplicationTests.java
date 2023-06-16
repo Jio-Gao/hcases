49232b0143e0265c847915982ccb7d5e5249fed0
Jio <gsgaohy@gmail.com>
2023-06-16 15:55:56 +0800

    Init

---
+++ c-spring-boot/src/test/java/com/hcases/cspringboot/CSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcases.cspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
